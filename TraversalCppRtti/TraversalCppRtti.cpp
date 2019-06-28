bf42073ff14caccfc3fbd5be8288d27d7f92106e
Kerisa <segistration@gmail.com>
2019-06-28 21:31:39 +0800

    整理

--- TraversalCppRtti/TraversalCppRtti.cpp
+++ TraversalCppRtti/TraversalCppRtti.cpp
@@ -55,77 +55,11 @@ typedef const struct _s__RTTIBaseClassDescriptor {
     __RTTIClassHierarchyDescriptor* pClassDescriptor;
 } __RTTIBaseClassDescriptor;
 
-string Dump(_s__RTTICompleteObjectLocator* rtti)
-{
-    string msg;
-    char buf[256];
-    sprintf_s(buf, sizeof(buf), R"(signature: %#x
-offset: %#x
-cdOffset: %#x
-TypeDescriptor:
-pVFTable: %#x
-spare: %#x
-name: %s
-)", rtti->signature, rtti->offset, rtti->cdOffset, (uint32_t)rtti->pTypeDescriptor->pVFTable, rtti->pTypeDescriptor->spare, rtti->pTypeDescriptor->name);
-    msg += buf;
-
-    if (!rtti->pClassHierarchyDescriptor && !IsBadReadPtr(rtti->pClassHierarchyDescriptor, 4))
-    {
-        msg += "RTTIClassHierarchyDescriptor: <null>";
-    }
-    else
-    {
-        // multiple inheritance, Bit 1: virtual inheritance
-        bool multiple = rtti->pClassHierarchyDescriptor->attributes & 0x1;
-        bool virt = rtti->pClassHierarchyDescriptor->attributes & 0x2;
-        string attrib = (multiple ? "<multiple inheritance>" : "");
-        attrib += (virt ? ", <virtual inheritance>" : "");
-        if (attrib.empty())
-            attrib = "<normal>";
-        sprintf_s(buf, sizeof(buf), R"(RTTIClassHierarchyDescriptor:
-signature: %#x
-attributes: %s
-numBaseClasses: %#x
-)", rtti->pClassHierarchyDescriptor->signature, attrib.c_str(), rtti->pClassHierarchyDescriptor->numBaseClasses);
-        msg += buf;
-        msg += "  Base Classes:\n";
-        for (int i = 0; i < rtti->pClassHierarchyDescriptor->numBaseClasses; ++i)
-        {
-            auto desc = rtti->pClassHierarchyDescriptor->pBaseClassArray->arrayOfBaseClassDescriptors[i];
-            sprintf_s(buf, sizeof(buf), R"(  <%x>
-pVFTable: %#x
-name: %s
-)", i + 1, (uint32_t)desc->pTypeDescriptor->pVFTable, desc->pTypeDescriptor->name);
-            msg += buf;
-        }
-    }
-    return msg;
-}
-
 uint32_t GetImageBaseInPEHeader(const vector<uint8_t>& peData)
 {
     PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)peData.data();
     PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)(peData.data() + pDosHeader->e_lfanew);
     return pNtHeader->OptionalHeader.ImageBase;
-}
-
-uint32_t GetFileAddrBelongSectionIndex(const vector<uint8_t>& peData, uint32_t addr)
-{
-    assert(addr < peData.size());
-    PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)peData.data();
-    PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)(peData.data() + pDosHeader->e_lfanew);
-    PIMAGE_SECTION_HEADER pSecHeader = (PIMAGE_SECTION_HEADER)(peData.data() + pDosHeader->e_lfanew + sizeof(IMAGE_NT_HEADERS));
-
-    for (int i = 0; i < pNtHeader->FileHeader.NumberOfSections; ++i)
-    {
-        if ((addr >= pSecHeader[i].PointerToRawData) &&
-            (addr <= pSecHeader[i].PointerToRawData + pSecHeader[i].SizeOfRawData))
-        {
-            // 内存偏移 = 所在区块的内存偏移 + （该偏移 - 所在区块的文件偏移）
-            return i;
-        }
-    }
-    return -1;
 }
 
 uint32_t RVAToFileOffset(const vector<uint8_t>& peData, uint32_t offset)
@@ -275,42 +209,6 @@ vector<const char*> FindAllReferenceInMem(const char* from, const char* to, cons
     return offsets;
 }
 
-#if 0
-vector<pair<uint32_t, string>> FindTypeDescriptor(const vector<uint8_t>& fileData)
-{
-    vector<pair<uint32_t, string>> offsets;
-    const char flag[] = { ".?AV" };
-    for (auto i = 0; i < fileData.size(); ++i)
-    {
-        const char* ptr = (const char*)&fileData[i];
-        if (!strncmp(flag, ptr, 4))
-        {
-            offsets.push_back(make_pair(i, ptr));
-            i += strlen(ptr) - 1;
-        }
-    }
-    return offsets;
-}
-#endif
-
-uint32_t FindExeImageBase()
-{
-    uint32_t p = reinterpret_cast<uint32_t>(FindExeImageBase);
-    p &= ~0xfff;
-    while (p)
-    {
-        __try
-        {
-            if (*(char*)p == 'M' && *(char*)(p + 1) == 'Z')
-                return p;
-        } __except (1) { }
-        p -= 0x1000;
-    }
-    return 0;
-}
-
-
-
 // return vtable rva
 std::vector<uint32_t> FindAllRttiInPEFile(const vector<uint8_t>& peData)
 {
@@ -327,18 +225,13 @@ std::vector<uint32_t> FindAllRttiInPEFile(const vector<uint8_t>& peData)
     {
         uint32_t peOffset = Detail::FileOffsetToRVA(peData, (uint32_t)nameAddr - findStart);
         uint32_t typeDesc = peOffset - CONTENT_OFFSET(Detail::_TypeDescriptor, name) + Detail::GetImageBaseInPEHeader(peData);
-        auto typeDescRef = FindAllReferenceInMem(findStart, findEnd, (uint32_t)typeDesc, true);
-        if (typeDescRef.empty())
-        {
-            int k = 0;
-        }
-        else
+        auto typeDescRef = FindAllReferenceInMem(findStart, findEnd, typeDesc, true);
+        if (!typeDescRef.empty())
         {
             cout << nameAddr << ": \n";
-            uint32_t rttiCOLFileOffset = 0;
             for (auto r : typeDescRef)
             {
-                rttiCOLFileOffset = r - findStart - CONTENT_OFFSET(Detail::_s__RTTICompleteObjectLocator, pTypeDescriptor);
+                uint32_t rttiCOLFileOffset = r - findStart - CONTENT_OFFSET(Detail::_s__RTTICompleteObjectLocator, pTypeDescriptor);
                 uint32_t hierDescVA = *(uint32_t*)(peData.data() + rttiCOLFileOffset + CONTENT_OFFSET(Detail::_s__RTTICompleteObjectLocator, pClassHierarchyDescriptor));
                 uint32_t hierDescFileOffset = Detail::RVAToFileOffset(peData, hierDescVA - imageBase);
                 if ((int)hierDescFileOffset < 0)  // 过滤无效结果
@@ -376,7 +269,7 @@ std::vector<uint32_t> FindAllRttiInPEFile(const vector<uint8_t>& peData)
                     {
                         uint32_t result_ctorRVA = Detail::FileOffsetToRVA(peData, vtableRefs[kk] - findStart);
 
-                        cout << "reference in ctors(next instruction): " << hex << result_ctorRVA + 4 << ", ";    // 显示下一条指令的地址
+                        cout << "reference in ctors(next instruction): " << hex << result_ctorRVA + sizeof(size_t) << ", ";    // 显示下一条指令的地址
                     }
                     cout << "\n";
                 }
@@ -387,105 +280,15 @@ std::vector<uint32_t> FindAllRttiInPEFile(const vector<uint8_t>& peData)
     return vector<uint32_t>();
 }
 
-
-class Animal01
-{
-public:
-    virtual void fff01() {}
-};
-class Animal02
-{
-public:
-    virtual void fff02() {}
-};
-class Animal11
-{
-public:
-    virtual void fff11() {}
-};
-class Animal12
-{
-public:
-    virtual void fff12() {}
-};
-class Animal0 : public Animal01, public Animal02
-{
-public:
-    virtual ~Animal0() {}
-};
-class Animal1 : public Animal11, public Animal12
-{
-public:
-    virtual void A1() {}
-};
-class Animal : virtual public Animal1, virtual public Animal0
-{
-public:
-    virtual ~Animal(void) {}
-};
-class Cat : public Animal {};
-class Dog : public Animal {};
-
-
-void ListExeRtti()
-{
-    uint32_t imagebase = FindExeImageBase();
-    vector<const char*> pos = FindAllReferenceInMem((char*)imagebase, (char*)(imagebase + 0x50000), ".?AV", 4);
-
-    for (int i = 0; i < pos.size(); ++i)
-    {
-        const char* addr = pos[i];
-
-        Detail::_TypeDescriptor* type = (Detail::_TypeDescriptor*)(addr - 8);
-        auto ref = FindAllReferenceInMem(imagebase, imagebase + 0x50000, (uint32_t)type, true);
-        if (ref.empty())
-        {
-            int k = 0;
-        }
-        else
-        {
-            Detail::_s__RTTICompleteObjectLocator* rtti = nullptr;
-            for (auto r : ref)
-            {
-                rtti = (Detail::_s__RTTICompleteObjectLocator*)(r - 12);
-                if (!IsBadReadPtr(rtti->pTypeDescriptor, sizeof(rtti->pTypeDescriptor)) && !IsBadReadPtr(rtti->pClassHierarchyDescriptor, sizeof(rtti->pClassHierarchyDescriptor)))
-                    break;
-            }
-            std::string msg = Detail::Dump(rtti);
-            cout << msg.c_str();
-            auto vtable = FindAllReferenceInMem(imagebase, imagebase + 0x50000, (uint32_t)rtti, true);
-            cout << "*vtable: ";
-            for (auto p : vtable)
-            {
-                char buf[32];
-                sprintf_s(buf, sizeof(buf), "%#x, ", p + 4);
-                cout << buf;
-            }
-            if (!vtable.empty())
-            {
-                auto ctors = FindAllReferenceInMem(imagebase, imagebase + 0x50000, vtable[0] + 4, false);
-                cout << "\n*referenced in ctors(next instruction): ";
-                for (auto p : ctors)
-                {
-                    char buf[32];
-                    sprintf_s(buf, sizeof(buf), "%#x, ", p + 4);
-                    cout << buf;
-                }
-            }
-            cout << "\n++++++++++++++++++++++++++\n";
-        }
-    }
-}
-
 int main(int argc, char** argv)
 {
-    Animal* d = new Dog();
-    Animal* a = new Cat();
-    Cat* c = dynamic_cast<Cat*>(a);
-    Animal* aa = new Animal();
-    Detail::__RTTICompleteObjectLocator* ptr = (Detail::__RTTICompleteObjectLocator*)(*(uint32_t**)aa)[-1];
-    dynamic_cast<Cat*>(aa);
-    vector<uint8_t> fileData = GetFileData(argc >= 2 ? argv[1] : argv[0]);
+    if (argc != 2)
+    {
+        cout << "usage: TraversalCppRtti.exe <PE file>\n";
+        return 0;
+    }
+
+    vector<uint8_t> fileData = GetFileData(argv[1]);
     FindAllRttiInPEFile(fileData);
     cout << "finish\n";
     return 0;
